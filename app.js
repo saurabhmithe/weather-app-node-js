151e042f513228290e2e56b5653c9a64e8b2a4ac
saurabhmithe <saurabhmithe@gmail.com>
2018-12-02 10:46:55 -0800

    using promises instead of callbacks

--- app.js
+++ app.js
@@ -16,20 +16,11 @@ argv;
 var address = argv.a;
 var lat, lon;
 
-geocode.geocodeAddress(address, (errorMessage, results) => {
-    if(errorMessage) {
-        console.log(errorMessage);
-    } else {
-        console.log(JSON.stringify(results, undefined, 4));
-
-        weather.getForecast(results.lat, results.lon, (errorMessage, weatherResults) => {
-            if(errorMessage) {
-                console.log(errorMessage);
-            } else {
-                console.log(JSON.stringify(weatherResults, undefined, 4));
-            }
-        });
-    }
+geocode.geocodeAddress(address).then((results) => {
+    console.log(JSON.stringify(results, undefined, 4));
+    return weather.getForecast(results.lat, results.lon);
+}).then((weatherResults) => {
+    console.log(JSON.stringify(weatherResults, undefined, 4));
+}).catch((errorMessage) => {
+    console.log(errorMessage);
 });
-
-// url: 'https://api.darksky.net/forecast/78de4c40a433dcef8701b952ff5e28ec/37.330910,-121.904997'
--- geocode/geocode.js
+++ geocode/geocode.js
@@ -1,30 +1,33 @@
 const request = require('request');
 
-var geocodeAddress = (address, callback) => {
-    var formattedAddress = encodeURIComponent(address.trim());
+var geocodeAddress = (address) => {
 
-    request({
-        url: `http://dev.virtualearth.net/REST/v1/Locations?q=${formattedAddress}&key=AvUg7T5TTyuHWQvN81XnrK_V5Ajuyd_BSzvD6e137SsK704drtRzDdQ1yiIXWOj5`,
-        json: true
-    }, (error, response, body) => {
-        if(error) {
-            callback('Unable to connect to the server.');
-        } else if (body.resourceSets[0].resources.length === 0) {
-            callback('Invalid address.');
-        } else if (response.statusCode == 200) {
-                var name = body.resourceSets[0].resources[0].name;
-                var lat = body.resourceSets[0].resources[0].point.coordinates[0];
-                var lon = body.resourceSets[0].resources[0].point.coordinates[1];
+    return new Promise((resolve, reject) => {
+        var formattedAddress = encodeURIComponent(address.trim());
 
-                callback(undefined, {
-                    name: name,
-                    lat: lat,
-                    lon: lon
-                });
+        request({
+            url: `http://dev.virtualearth.net/REST/v1/Locations?q=${formattedAddress}&key=AvUg7T5TTyuHWQvN81XnrK_V5Ajuyd_BSzvD6e137SsK704drtRzDdQ1yiIXWOj5`,
+            json: true
+        }, (error, response, body) => {
+            if(error) {
+                reject('Unable to connect to the server.');
+            } else if (body.resourceSets[0].resources.length === 0) {
+                reject('Invalid address.');
+            } else if (response.statusCode == 200) {
+                    var name = body.resourceSets[0].resources[0].name;
+                    var lat = body.resourceSets[0].resources[0].point.coordinates[0];
+                    var lon = body.resourceSets[0].resources[0].point.coordinates[1];
 
-        } else {
-            callback('Something went wrong.');
-        }
+                    resolve({
+                        name: name,
+                        lat: lat,
+                        lon: lon
+                    });
+
+            } else {
+                reject('Something went wrong.');
+            }
+        });
     });
 }
 
